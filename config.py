c90df73cde554604818d44a358cb7ea38d08d1e3
Mario Cruz <mario@mariocruz.net>
2024-12-28 12:11:37 -0500

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,3 @@ WIFI_PASSWORD = "Password"
 
 # Image Management Settings
 MAX_SAVED_IMAGES = 3  # Number of images to keep in storage
-
-# Watchdog Settings
-ENABLE_WATCHDOG = False
-WATCHDOG_TIMEOUT = 8000  # milliseconds
